5bf7cff9fd5586bb3dc67c08043902fac1589b9f
thq1995 <anhtuanquang2201@gmail.com>
2024-10-25 20:24:29 -0700

    update grafana

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import GlobalSettings
 from app.api.router import api_router
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Document RAG API")
+
+Instrumentator().instrument(app).expose(app)
 
 app.add_middleware(
     CORSMiddleware,
